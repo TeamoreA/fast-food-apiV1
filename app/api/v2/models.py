25248764bf49ee9b79957bf2d29919c408b80cbe
Teamo <teamorekamau97@gmail.com>
2018-09-27 13:24:09 +0300

    Fundtionality to update the status of an order [#160813531]

--- app/api/v2/models.py
+++ app/api/v2/models.py
@@ -6,7 +6,9 @@ import jwt
 import psycopg2
 
 
-conn = psycopg2.connect(os.getenv('DATABASE_URL'))
+# conn = psycopg2.connect(os.getenv('DATABASE_URL'))
+conn = psycopg2.connect(
+    "dbname='fooddb' host= '127.0.0.1' port='5432' user='postgres' password=''")
 cur = conn.cursor()
 
 
@@ -28,12 +30,16 @@ def token(f):
         try:
             data = jwt.decode(token, os.getenv('SECRET'))
             cur.execute('SELECT * FROM users WHERE id = (%s);', (data['id'],))
-            active_user = cur.fetchone()
-            # user = [user for user in USERS if user['id'] == data['id']]
-            # active_user = user[0]
+            user = cur.fetchone()
+            active_user = {}
+            active_user['id'] = user[0]
+            active_user['name'] = user[1]
+            active_user['password'] = user[2]
+            active_user['admin'] = user[3]
+
         except:
             response = jsonify({'message': 'Tokens do not match or expired!'})
             response.status_code = 401
             return response
-        return f(active_user, *args, **kwargs)
+        return f(*args, active_user, **kwargs)
     return wrapped_func
